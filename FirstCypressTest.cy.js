5413494b99086711308510798523a0bcb9550d74
Prathibha <prathibha.r@gmail.com>
2024-09-09 19:05:42 -0600

    Initial Commit

---
+++ FirstCypressTest.cy.js
@@ -0,0 +1,12 @@
+describe('Test Suite1', () => {
+  it('My First Test', () => {
+    cy.log("Test1 has started");
+    cy.visit('https://www.youtube.com/')
+    cy.log("Navigated to Youtube");
+    cy.get('input[id="search"]').type("Java Script by Testers Talk");
+    cy.get('button[id="search-icon-legacy"]>yt-icon').click();
+    cy.log("Test1 is completed")
+
+
+  })
+})
